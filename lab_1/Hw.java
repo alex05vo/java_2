4c5bb24cf90a4382aef91a10e29970aa737691b7
alex05vo <alex05.vo@gmail.com>
2025-10-13 16:49:49 +0300

    first

---
+++ lab_1/Hw.java
@@ -0,0 +1,6 @@
+public class Hw {
+    
+    public void Hello() {
+        System.out.println("Hello, World! My name is Oleksandr");
+    }
+}
---
+++ lab_1/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            String userInput;
+
+            while (true) {
+
+                System.out.println("Введiть номер завдання (1), або 'q' для виходу:");
+                userInput = scanner.nextLine();
+
+                if (userInput.equalsIgnoreCase("q")) {
+                    return;
+                }
+
+                switch (userInput) {
+                    case "1":
+                        System.out.println("-------------------------------");
+                        Hw hw = new Hw();
+                        hw.Hello();
+                        break;
+                    default:
+                        System.out.println("Неправильний ввiд. Введiть номер завдання (1; 2; 3; 4) або 'q' для виходу.");
+                        break;
+                }
+            }
+        }
+    }
+}
